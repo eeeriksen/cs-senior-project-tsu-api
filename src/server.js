f4683e2a9e4d7ee6d7bcc878d2cfaf4ce292bf08
Eriksen Lezama <eriksen.lezama@gmail.com>
2024-10-12 19:29:35 -0500

    add 404 and if for OPTIONS

--- src/server.js
+++ src/server.js
@@ -8,11 +8,11 @@ import { sendRecommendation } from './controllers/recommendationController.js'
 const app = express()
 
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'http://localhost:5173')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
     if (req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'http://localhost:5173')
-        res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-        res.setHeader('Access-Control-Allow-Credentials', 'true')
         return res.sendStatus(200)
     }
     next()
@@ -23,8 +23,11 @@ app.use(express.json())
 app.use('/post', postRoutes)
 app.use('/user', userRoutes)
 app.use('/comment', commentRoutes)
-
 app.post('/send-recommendation', sendRecommendation)
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' })
+})
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
